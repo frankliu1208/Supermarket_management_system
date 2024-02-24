a9a74e8e5f1c2a3295b5e7ae3969bf71e2c419cc
liuhang6666 <liuhang@oregonstate.edu>
2024-02-24 18:05:45 +0100

    Category main feature completed

--- WebApp/Controllers/CategoriesController.cs
+++ WebApp/Controllers/CategoriesController.cs
@@ -1,26 +1,61 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
 
 namespace WebApp.Controllers
 {
     public class CategoriesController : Controller
     {
+        // displaying a list of categories
         public IActionResult Index()
+        {
+            var categories = CategoriesRespository.GetCategories();
+            return View(categories);
+        }
+
+        // editing a category
+        public IActionResult Edit(int? id)
+        {
+            var category = CategoriesRespository.GetCategoryById(id.HasValue ? id.Value : 0);
+
+            return View(category);
+
+        }
+
+        // invoked when the form for editing a category is submitted, use model binding to receive the data 
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                CategoriesRespository.UpdateCategory(category.CategoryId, category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // display a form for adding a new category
+        public IActionResult Add()
         {
             return View();
         }
 
-        public IActionResult Edit(int? id)
-        {
-            if (id.HasValue)
-            {
-                return new ContentResult { Content = id.ToString()};
 
-            }
-            else
+        [HttpPost]
+        public IActionResult Add(Category category)
+        {
+            if (ModelState.IsValid)
             {
-                return new ContentResult { Content = "No content" };
+                CategoriesRespository.AddCategory(category);
+                return RedirectToAction(nameof(Index));
             }
+            return View(category);
         }
 
+        public IActionResult Delete(int categoryId)
+        {
+            CategoriesRespository.DeleteCategory(categoryId);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
---
+++ WebApp/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        // validation needed,  name shall not be empty
+        [Required] 
+        public string Name { get; set; } = string.Empty; 
+        public string? Description { get; set; } = string.Empty;
+    }
+}
