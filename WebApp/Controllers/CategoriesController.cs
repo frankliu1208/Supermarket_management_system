af79aa31a099ce085208fb90249f86557f7e0fc6
liuhang6666 <liuhang@oregonstate.edu>
2024-03-08 19:31:13 +0100

    ongoing: refactor

--- WebApp/Controllers/CategoriesController.cs
+++ WebApp/Controllers/CategoriesController.cs
@@ -1,57 +1,72 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using WebApp.Models;
-
+using UseCases.CategoriesUseCases;
+using CoreBusiness;
 
 namespace WebApp.Controllers
 {
     public class CategoriesController : Controller
     {
-        // displaying a list of categories
+        private readonly IViewCategoriesUseCase viewCategoriesUseCase;
+        private readonly IViewSelectedCategoryUseCase viewSelectedCategoryUseCase;
+        private readonly IEditCategoryUseCase editCategoryUseCase;
+        private readonly IAddCategoryUseCase addCategoryUseCase;
+        private readonly IDeleteCategoryUseCase deleteCategoryUseCase;
+
+        public CategoriesController(
+            IViewCategoriesUseCase viewCategoriesUseCase,
+            IViewSelectedCategoryUseCase viewSelectedCategoryUseCase,
+            IEditCategoryUseCase editCategoryUseCase,
+            IAddCategoryUseCase addCategoryUseCase,
+            IDeleteCategoryUseCase deleteCategoryUseCase)
+        {
+            this.viewCategoriesUseCase = viewCategoriesUseCase;
+            this.viewSelectedCategoryUseCase = viewSelectedCategoryUseCase;
+            this.editCategoryUseCase = editCategoryUseCase;
+            this.addCategoryUseCase = addCategoryUseCase;
+            this.deleteCategoryUseCase = deleteCategoryUseCase;
+        }
+
         public IActionResult Index()
         {
-            var categories = CategoriesRepository.GetCategories();
+            var categories = viewCategoriesUseCase.Execute();
             return View(categories);
         }
 
-        // editing a category
         public IActionResult Edit(int? id)
         {
             ViewBag.Action = "edit";
 
-            var category = CategoriesRepository.GetCategoryById(id.HasValue ? id.Value : 0);
+            var category = viewSelectedCategoryUseCase.Execute(id.HasValue ? id.Value : 0);
 
             return View(category);
-
         }
 
-        // invoked when the form for editing a category is submitted, use model binding to receive the data 
         [HttpPost]
         public IActionResult Edit(Category category)
         {
             if (ModelState.IsValid)
             {
-                CategoriesRepository.UpdateCategory(category.CategoryId, category);
+                editCategoryUseCase.Execute(category.CategoryId, category);
                 return RedirectToAction(nameof(Index));
             }
+
             ViewBag.Action = "edit";
             return View(category);
         }
 
-        // display a form for adding a new category
         public IActionResult Add()
         {
             ViewBag.Action = "add";
-             
+
             return View();
         }
-
 
         [HttpPost]
         public IActionResult Add(Category category)
         {
             if (ModelState.IsValid)
             {
-                CategoriesRepository.AddCategory(category);
+                addCategoryUseCase.Execute(category);
                 return RedirectToAction(nameof(Index));
             }
 
@@ -61,8 +76,9 @@ namespace WebApp.Controllers
 
         public IActionResult Delete(int categoryId)
         {
-            CategoriesRepository.DeleteCategory(categoryId);
+            deleteCategoryUseCase.Execute(categoryId);
             return RedirectToAction(nameof(Index));
         }
+
     }
 }
